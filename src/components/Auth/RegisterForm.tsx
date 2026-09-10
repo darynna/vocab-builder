4cd5b55669a7b7d91dbb2448acb263270399869d
Daryna Holikova <dgolikova0@gmail.com>
2026-09-10 10:07:58 +0100

    style: add responsive register page

--- src/components/Auth/RegisterForm.tsx
+++ src/components/Auth/RegisterForm.tsx
@@ -19,27 +19,27 @@ const RegisterForm = () => {
         /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
         <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-3.5 w-full'>
             <label><input {...register("name", {required: "Name is required"})} type='text' placeholder="Name"
-                          className='block w-full p-4 text-base border-2 border-grey-background rounded-xl'/>
+                          className='block w-full p-4 text-base border border-black/10 rounded-xl'/>
                 {errors.name && (
                     <span className='text-xs text-red-900'>{errors.name.message}</span>
                 )}</label>
             <label>
                 <input {...register("email", {required: "Email is required"})} type='email' placeholder="Email"
-                       className='block w-full p-4 text-base border-2 border-grey-background rounded-xl'/>
+                       className='block w-full p-4 text-base border border-black/10 rounded-xl'/>
                 {errors.email && (
                     <span className='text-xs text-red-900'>{errors.email.message}</span>
                 )}
             </label>
             <label>
                 <input {...register("password", {required: "Password is required"})} type='password' placeholder="Password"
-                       className='block w-full p-4 text-base border-2 border-grey-background rounded-xl'/>
+                       className='block w-full p-4 text-base border border-black/10 rounded-xl'/>
                 {errors.password && (
                     <span className='text-xs text-red-900'>{errors.password.message}</span>
                 )}
             </label>
 
 
-            <button type="submit" className='bg-green-accent text-white p-4 rounded-4xl'>Register</button>
+            <button type="submit" className='mt-4 bg-green-accent text-white p-4 rounded-4xl md:text-lg'>Register</button>
         </form>
     )
 };
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,7 +1,32 @@
+import RegisterForm from "../components/Auth/RegisterForm.tsx";
+import {NavLink} from "react-router-dom";
+
 const RegisterPage = () => {
-  return (
-    <div>RegisterPage</div>
-  );
+    return (
+        <div className='flex flex-col justify-center items-center min-h-screen mx-auto'>
+            <img src="/images/illustration.png" alt="kids" className="w-[247px] md:hidden"/>
+            <div className="flex flex-1 md:flex-0 flex-col justify-center -m-4 max-w-[628px] bg-green-accent/10 rounded-t-4xl md:rounded-4xl">
+                <div className='px-4 py-8 md:py-12 md:px-16'>
+                    <h1 className="text-3xl md:text-4xl mb-4 font-semibold">Register</h1>
+                    <p className="text-gray-600 mb-4 md:text-xl">To start using our services, please fill out the registration form
+                        below. All fields are mandatory:</p>
+                    <div className="flex flex-col gap-4 justify-center items-center">
+                        <RegisterForm/>
+                        <NavLink to="/login" className="text-base opacity-50 underline">Login</NavLink>
+                    </div>
+                </div>
+            </div>
+            <div className="hidden md:flex items-center md:mt-[98px] gap-2 text-base">
+                <span>Word</span>
+                <span>·</span>
+                <span>Translation</span>
+                <span>·</span>
+                <span>Grammar</span>
+                <span>·</span>
+                <span>Progress</span>
+            </div>
+        </div>
+    );
 };
 
 export default RegisterPage;
